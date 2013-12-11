71a9ebdfa2fd9d7b08b4ad9f849ec05a48f8884a
Takahiro HIMURA <taka@himura.jp>
2013-12-11 17:36:43 +0900

    improve spec helper

--- spec/helper.rb
+++ spec/helper.rb
@@ -19,4 +19,39 @@ VCR.configure do |c|
   c.configure_rspec_metadata!
   c.hook_into :webmock
   c.allow_http_connections_when_no_cassette = true
+
+  c.filter_sensitive_data("<ACCESS_KEY>") do
+    test_access_key
+  end
+  c.filter_sensitive_data("<SECRET_KEY>") do
+    test_secret_key
+  end
+  c.filter_sensitive_data("<GP_SERVICE_CODE>") do
+    test_gp_service_code
+  end
+  c.filter_sensitive_data("<GP_ENDPOINT>") do
+    test_gp_endpoint
+  end
+end
+
+def test_access_key
+  ENV.fetch('IIJAPI_TEST_ACCESS_KEY')
+end
+
+def test_secret_key
+  ENV.fetch('IIJAPI_TEST_SECRET_KEY')
+end
+
+def test_gp_service_code
+  ENV.fetch('IIJAPI_TEST_GP_SERVICE_CODE')
+end
+
+def test_gp_endpoint
+  ENV.fetch('IIJAPI_TEST_GP_ENDPOINT')
+end
+
+def signed_gp_client
+  IIJAPI::GP::Client.new(:endpoint => test_gp_endpoint,
+                         :access_key => test_access_key,
+                         :secret_key => test_secret_key)
 end
